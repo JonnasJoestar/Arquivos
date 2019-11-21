d30bb6b01d2f1126768eedc403be1839ebfa1463
JonnasJoestar <56116831+JonnasJoestar@users.noreply.github.com>
2019-11-20 21:36:30 -0300

    Add files via upload

--- Lista Complementar/src/ExO.java
+++ Lista Complementar/src/ExO.java
@@ -2,31 +2,29 @@ import java.util.Scanner;
 public class ExO {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int a[],b[],c[],i;
+		int a[],b[],c[],i,j,co=0;
 		a = new int[5];
 		b = new int[5];
 		c = new int[5];
 		for(i = 0; i < a.length;i++) {
 			a[i] = in.nextInt();
 		}
-		b[0] = a[0];
-		c[0] = a[0];
 		
-			for(int j = 0; j < b.length; j++) {
-				if(a[j] < b[j]) {
-					b[j] = a[j];
-				}else if(a[j] > c[j]) {
-					c[j] = a[j];
+		c[0] = 0;
+		b[0] = a[0];
+		
+		for(i = 0; i < a.length; i++) {
+			for(j = 0; j < b.length; j++) {
+				if(b[j] > a[i]) {
+					b[j] = a[i];
+				}else if(c[j] > a[i]) {
+					c[j] = a[i];
 				}
 			}
+		}
 		
-		System.out.println("Ordem Crescente");
 		for(i = 0; i < b.length; i++) {
 			System.out.println(b[i]);
-		}
-		System.out.println("Ordem Decrescente");
-		for(i = 0; i < c.length; i++) {
-			System.out.println(c[i]);
 		}
 	}
 }
